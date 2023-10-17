5c316cd3e2bb839c1f9a9df77721b2bb2669be78
SAbdukodirov <abdukodirovsamandar8@gmail.com>
2023-10-17 15:21:46 +0500

    alsjdalksdaasda

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int currentNumber = 0;
 
+        while (currentNumber <= printToInclusive) {
+            long factorial = 1;
+            int temp = currentNumber;
+
+            while (temp > 1) {
+                factorial *= temp;
+                temp--;
+            }
+
+            System.out.println("Factorial of " + currentNumber + ": " + factorial);
+            currentNumber++;
+        }
     }
 }
